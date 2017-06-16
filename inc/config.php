f4f9898a96d678f77774ab1c52865f93f29ff872
Subin Siby <subins2000@gmail.com>
2017-06-16 15:44:04 +0530

    Remove $dbh checking

--- inc/config.php
+++ inc/config.php
@@ -1,16 +1,15 @@
 <?php
 // ini_set("display_errors","on");
-if (!isset($dbh)) {
-    date_default_timezone_set('UTC'); // Set Time Zone
 
-    $username = getenv('OPENSHIFT_MYSQL_DB_USERNAME');
-    $password = getenv('OPENSHIFT_MYSQL_DB_PASSWORD');
-    $hostname = getenv('OPENSHIFT_MYSQL_DB_HOST');
-    $dbname   = getenv('OPENSHIFT_GEAR_NAME');
-    $port     = getenv('OPENSHIFT_MYSQL_DB_PORT');
-    $dbh      = new PDO('mysql:dbname=' . $dbname . ';host=' . $hostname . ';port=' . $port, $username, $password);
+date_default_timezone_set('UTC'); // Set Time Zone
 
-    $siteHostname = 'demos.sim';
-    $siteURL      = 'https://demos.sim';
-    $blogURL      = 'https://subinsb.com';
-}
+$username = getenv('OPENSHIFT_MYSQL_DB_USERNAME');
+$password = getenv('OPENSHIFT_MYSQL_DB_PASSWORD');
+$hostname = getenv('OPENSHIFT_MYSQL_DB_HOST');
+$dbname   = getenv('OPENSHIFT_GEAR_NAME');
+$port     = getenv('OPENSHIFT_MYSQL_DB_PORT');
+$dbh      = new PDO('mysql:dbname=' . $dbname . ';host=' . $hostname . ';port=' . $port, $username, $password);
+
+$siteHostname = 'demos.sim';
+$siteURL      = 'https://demos.sim';
+$blogURL      = 'https://subinsb.com';
