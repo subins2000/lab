d480b61c00dd139eb6d98c93f0653f8a43a48c7c
Subin Siby <subins2000@gmail.com>
2017-11-05 20:21:30 +0530

    Fix bug

--- public/isl-profile-pic/getFBImage.php
+++ public/isl-profile-pic/getFBImage.php
@@ -4,5 +4,5 @@ header('Content-Type: image/jpeg');
 if (isset($_GET['at'])) {
     $url = 'https://graph.facebook.com/v2.7/me/picture?access_token=' . htmlspecialchars($_GET['at']) . '&height=640';
 
-    echo file_get_contents($j['data']['url']);
+    echo file_get_contents($url);
 }
