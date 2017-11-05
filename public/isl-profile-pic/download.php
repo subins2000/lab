3e9683c59ddd14e94a600188b0ce1a11a3672950
Subin Siby <subins2000@gmail.com>
2017-11-05 13:31:46 +0530

    Stable version

--- public/isl-profile-pic/download.php
+++ public/isl-profile-pic/download.php
@@ -1,41 +1,49 @@
+<?php
+require_once __DIR__ . '/../../inc/load.php';
+?>
 <!DOCTYPE html>
 <html xmlns="http://www.w3.org/1999/xhtml" xmlns:og="http://ogp.me/ns#" xmlns:fb="http://www.facebook.com/2008/fbml">
-  <head>
-    <title>Download Profile Picture | Support Kerala Blasters !</title>
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link href="css/style.css" rel="stylesheet" async="async" />
-    <meta property="og:title" content="Support Kerala Blasters!" />
-    <meta property="og:url" content="http://demos.subinsb.com/isl-profile-pic/" />
-    <meta property="og:image" content="http://demos.subinsb.com/isl-profile-pic/image/og-logo.png" />
+    <head>
+        <title>Download Profile Picture | Support Kerala Blasters !</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link href="css/style.css" rel="stylesheet" async="async" />
+        <meta property="og:title" content="Support Kerala Blasters!" />
+        <meta property="og:url" content="http://demos.subinsb.com/isl-profile-pic/" />
+        <meta property="og:image" content="http://demos.subinsb.com/isl-profile-pic/image/og-logo.png" />
 
-    <meta name="description" content="Support Kerala Blasters by changing your profile picture. You can directly upload 
-    it to Facebook or download the image." />
-    <meta property="og:description" content="Support Kerala Blasters by changing your profile picture. You can directly upload it to Facebook or download the image." />
+        <meta name="description" content="Support Kerala Blasters by changing your profile picture. You can directly upload
+        it to Facebook or download the image." />
+        <meta property="og:description" content="Support Kerala Blasters by changing your profile picture. You can directly upload it to Facebook or download the image." />
 
-    <meta property="fb:app_id" content="1046604178789662" />
-  </head>
-  <body>
-    <div id="wrapper">
-      <div class="container" id="content">
-        <h1><a href="<?php echo $siteURL;?>/isl-profile-pic/">Support Kerala Blasters!</a></h1>
-        <p>Make this as your profile picture to support Blasters !</p>
-        <?php
-        $url = htmlspecialchars($_GET["i"]);
-        if(isset($_GET["i"]))
-          echo "<a href='". $url ."' download='kerala-blasters-profile'><img src='". $url ."' /></a>";
-        else
-          header("Redirect: index.php");
-        ?>
-        <p>
-        Click the above image to download.<br/>Or right click (in mobile, hold down) and choose "Save Image"
-        </p>
-        <p>
-          <a href="<?php echo $siteURL;?>/isl-profile-pic/"><button id="download">Create Another Profile Picture!</button></a>
-        </p>
-        <?php
-        require "footer.php"
-        ?>
-      </div>
-    </div>
-  </body>
+        <meta property="fb:app_id" content="1046604178789662" />
+    </head>
+    <body>
+        <div id="wrapper">
+            <div id="header">
+                <h1><a href="<?php echo $siteURL;?>/isl-profile-pic/">Support Kerala Blasters!</a></h1>
+                <p>Support Kerala Blasters by changing your profile picture</p>
+            </div>
+            <div class="container" id="content">
+                <p>Make this as your profile picture to support Blasters !</p>
+                <?php
+                $url = htmlspecialchars($_GET['i']);
+                if (isset($_GET['i'])) {
+                        echo "<a href='" . $url . "' download='kerala-blasters-profile'><img src='" . $url . "' /></a>";
+                } else {
+                        header('Redirect: index.php');
+                }
+
+                ?>
+                <p>
+                    Click the above image to download.<br/>Or hold down on image and choose "Save Image"
+                </p>
+                <p>
+                    <a href="<?php echo $siteURL; ?>/isl-profile-pic/"><button class="btn">Create Another Profile Picture!</button></a>
+                </p>
+                <?php
+                require 'footer.php'
+                ;?>
+            </div>
+        </div>
+    </body>
 </html>
--- public/isl-profile-pic/upload.php
+++ public/isl-profile-pic/upload.php
@@ -1,31 +1,31 @@
 <?php
-if(isset($_FILES["image"])){
-  require_once "convert.php";
-  function rand_string($length) {
-     $str="";
-     $chars = "subinsblogabcdefghijklmanopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
-     $size = strlen($chars);
-     for($i = 0;$i < $length;$i++) {
-      $str .= $chars[rand(0,$size-1)];
+if (isset($_FILES['image'])) {
+    require_once __DIR__ . '/inc/convert.php';
+    function randString($length)
+    {
+        $str   = '';
+        $chars = 'subinsblogabcdefghijklmanopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
+        $size  = strlen($chars);
+        for ($i = 0; $i < $length; $i++) {
+            $str .= $chars[rand(0, $size - 1)];
+        }
+        return $str;
     }
-    return $str;
-  }
 
-  $sourceImg = @imagecreatefromstring(@file_get_contents($_FILES["image"]["tmp_name"]));
-  if ($sourceImg === false){
-    echo "image/og-logo.png";
-    exit;
-  }
+    $sourceImg = @imagecreatefromstring(@file_get_contents($_FILES['image']['tmp_name']));
+    if ($sourceImg === false) {
+        echo 'image/og-logo.png';
+        exit;
+    }
 
-  $image = makeDP(file_get_contents($_FILES["image"]["tmp_name"]), (
-    isset($_POST["design"]) ? $_POST["design"] : null
-  ));
+    $image = makeDP($_FILES['image']['tmp_name'], (
+        isset($_POST['design']) ? $_POST['design'] : null
+    ));
 
-  $loc = "uploads/" . rand_string(10) . ".png";
+    $loc = 'uploads/' . randString(10) . '.png';
 
-  file_put_contents($loc, $image);
-  echo $loc;
+    file_put_contents($loc, $image);
+    echo $loc;
 
-  imagedestroy($dest);
-  imagedestroy($src);
+    imagedestroy($sourceImg);
 }
