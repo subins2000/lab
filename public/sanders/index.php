8d90bb35f06638fbfebc52de50c2bf3d27b832c4
Subin Siby <subins2000@gmail.com>
2019-07-07 02:59:55 +0530

    Add stats page

--- public/sanders/index.php
+++ public/sanders/index.php
@@ -1,1 +1,22 @@
- 
+<?php
+require_once __DIR__ . '/../../inc/load.php';
+require_once __DIR__ . '/phpcount.php';
+
+PHPCount::setDBAdapter($dbh);
+?>
+<table>
+    <tbody>
+        <?php
+        $sth = $dbh->query('SELECT DISTINCT pageid FROM `hits`');
+        while($r = $sth->fetch()) {
+            echo '<tr>';
+                echo '<td>' . $r['pageid'] . '</td>';
+                echo '<td>' . PHPCount::GetHits($r['pageid']) . '</td>';
+            echo '</tr>';
+        }
+        ?>
+    </tbody>
+</table>
+<?php
+echo "<b>Total hitcount</b> : " . PHPCount::GetTotalHits();
+?>
