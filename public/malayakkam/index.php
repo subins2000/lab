584fb93579fe7abb3c77532bf79ea095482adf94
Subin Siby <mail@subinsb.com>
2020-04-12 15:07:13 +0530

    fix text

--- public/malayakkam/index.php
+++ public/malayakkam/index.php
@@ -14,15 +14,15 @@ init(48);
             <h5>Encode</h5>
             <p>Convert 0, 1, 2 etc.. numbers to its Malayalam equivalent ൦, ൧, ൨. Note: This just replaces the numerals and is not according to the <a href="https://en.wikipedia.org/wiki/Malayalam_script#Other_symbols">numeric grammar of Malayalam</a>.</p>
             <form>
-                <textarea class="materialize-textarea" id="വാക്യം" placeholder="ഇംഗ്ലീഷ് അക്കമുള്ള വാക്യം ഇവിടെ എഴുതുക... Write English numerals here..."></textarea>
+                <textarea class="materialize-textarea" id="വാക്യം" placeholder="ഹിന്ദു-അറബിക്ക് അക്കമുള്ള വാക്യം ഇവിടെ എഴുതുക... Write English numerals here..."></textarea>
             </form>
             <textarea class="materialize-textarea" id="മാറ്റിയവാക്യം" placeholder="മലയാള അക്കത്തിലേക്ക് മാറ്റിയ വാക്യം ഇവിടെ ദൃശ്യമാകും... Converted Malayalam numerals equivalent will be displayed here..." readonly="readonly"></textarea>
             <h5>Decode</h5>
             <p>Convert Malayalam numbers to its Hindu-Arabic numerals equivalent </p>
             <form>
-                <textarea class="materialize-textarea" id="മലയാളവാക്യം" placeholder="ഇംഗ്ലീഷ് അക്കമുള്ള വാക്യം ഇവിടെ എഴുതുക... Write English numerals here..."></textarea>
+                <textarea class="materialize-textarea" id="മലയാളവാക്യം" placeholder="ഹിന്ദു-അറബിക്ക് അക്കമുള്ള വാക്യം ഇവിടെ എഴുതുക... Write English numerals here..."></textarea>
             </form>
-            <textarea class="materialize-textarea" id="ഇംഗ്ലീഷിലേക്ക്മാറ്റിയവാക്യം" placeholder="മലയാള അക്കത്തിലേക്ക് മാറ്റിയ വാക്യം ഇവിടെ ദൃശ്യമാകും... Converted Malayalam numerals equivalent will be displayed here..." readonly="readonly"></textarea>
+            <textarea class="materialize-textarea" id="ഹിന്ദുഅറബിക്കിലേക്ക്മാറ്റിയവാക്യം" placeholder="മലയാള അക്കത്തിലേക്ക് മാറ്റിയ വാക്യം ഇവിടെ ദൃശ്യമാകും... Converted Malayalam numerals equivalent will be displayed here..." readonly="readonly"></textarea>
             <script type="text/javascript" src="//lab.subinsb.com/projects/thengascript/thengascript.js"></script>
             <script type="text/thengascript" src="//lab.subinsb.com/projects/thengascript/malayakkam.js"></script>
             <script type="text/thengascript">
@@ -32,9 +32,9 @@ init(48);
                     മാറ്റിയവാക്യസ്ഥലം.value = മലയക്കം(വാക്യസ്ഥലം.value);
                 });
                 ആവട്ടെ മലയാളവാക്യസ്ഥലം = document.getElementById('മലയാളവാക്യം'),
-                      ഇംഗ്ലീഷിലേക്ക്മാറ്റിയവാക്യസ്ഥലം = document.getElementById('ഇംഗ്ലീഷിലേക്ക്മാറ്റിയവാക്യം');
+                      ഹിന്ദുഅറബിക്കിലേക്ക്മാറ്റിയവാക്യസ്ഥലം = document.getElementById('ഹിന്ദുഅറബിക്കിലേക്ക്മാറ്റിയവാക്യം');
                 മലയാളവാക്യസ്ഥലം.addEventListener('keyup', () => {
-                    ഇംഗ്ലീഷിലേക്ക്മാറ്റിയവാക്യസ്ഥലം.value = മലയക്കം(മലയാളവാക്യസ്ഥലം.value, ശരി);
+                    ഹിന്ദുഅറബിക്കിലേക്ക്മാറ്റിയവാക്യസ്ഥലം.value = മലയക്കം(മലയാളവാക്യസ്ഥലം.value, ശരി);
                 });
             </script>
             <style>
