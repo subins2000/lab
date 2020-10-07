fbb69c382d55b96d734ce8ab8fe3789dca9d1810
Subin Siby <mail@subinsb.com>
2020-10-07 19:26:36 +0530

    Show exception

--- public/sanders/index.php
+++ public/sanders/index.php
@@ -7,13 +7,20 @@ PHPCount::setDBAdapter($dbh);
 <table>
     <tbody>
         <?php
-        $sth = $dbh->query('TRUNCATE nodupes;');
-        $sth = $dbh->query('SELECT DISTINCT pageid FROM `hits` ORDER BY `hits` LIMIT 20');
-        while($r = $sth->fetch()) {
-            echo '<tr>';
-                echo '<td>' . htmlspecialchars($r['pageid']) . '</td>';
-                echo '<td>' . PHPCount::GetHits($r['pageid']) . '</td>';
-            echo '</tr>';
+        $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        $dbh->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
+
+        try {
+            $sth = $dbh->query('TRUNCATE nodupes;');
+            $sth = $dbh->query('SELECT DISTINCT pageid FROM `hits` ORDER BY `hits` LIMIT 20');
+            while($r = $sth->fetch()) {
+                echo '<tr>';
+                    echo '<td>' . htmlspecialchars($r['pageid']) . '</td>';
+                    echo '<td>' . PHPCount::GetHits($r['pageid']) . '</td>';
+                echo '</tr>';
+            }
+        } catch(Exception $e) {
+            var_dump($e->getMessage());
         }
         ?>
     </tbody>
